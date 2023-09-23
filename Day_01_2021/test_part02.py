2ec3d6d18a46733440ca8a95408c265f71ac830d
Osama Beshir <osamabeshir4@gmail.com>
2023-09-23 23:15:19 +0200

    Some more changes to the test file

--- Day_01_2021/test_part02.py
+++ Day_01_2021/test_part02.py
@@ -1,52 +1,43 @@
 import pytest
+import os 
 
-from cachetools import cached, LRUCache
+# Get the directory of the current script
+script_dir = os.path.dirname(__file__)
 
-cache = LRUCache(maxsize=100)
+# Specify the full path to 'input.txt'
+input_file_path = os.path.join(script_dir, 'input.txt')
 
-@cached(cache=cache)
-def load_input_file():
-  with open('input.txt', 'r') as f:
+with open(input_file_path, 'r') as f:
     depth_measurements = [int(line) for line in f]
-    return depth_measurements
-
 
 def count_depth_increases_sliding_window(depth_measurements):
+    sums = []
+    for i in range(len(depth_measurements) - 2):
+        sum_ = depth_measurements[i] + depth_measurements[i + 1] + depth_measurements[i + 2]
+        sums.append(sum_)
 
-  sums = []
-  for i in range(len(depth_measurements) - 2):
-    sum = depth_measurements[i] + depth_measurements[i + 1] + depth_measurements[i + 2]
-    sums.append(sum)
+    count = 0
+    for i in range(1, len(sums)):
+        if sums[i] > sums[i - 1]:
+            count += 1
 
-  count = 0
-  for i in range(1, len(sums)):
-    if sums[i] > sums[i - 1]:
-      count += 1
-
-  return count
-
+    return count
 
 @pytest.fixture
 def example_input():
-  return [3]
-
+    return [3]
 
 @pytest.fixture
 def real_input():
-  return load_input_file()
-
+    return depth_measurements 
 
 def test_example_input(example_input):
-  expected_count = 0
-
-  actual_count = count_depth_increases_sliding_window(example_input)
-
-  assert expected_count == actual_count
-
+    expected_count = 0
+    actual_count = count_depth_increases_sliding_window(example_input)
+    assert expected_count == actual_count
 
 def test_real_input(real_input):
-  expected_count = 1523
-
-  actual_count = count_depth_increases_sliding_window(real_input)
-
-  assert expected_count == actual_count
+    expected_count = 1523
+    actual_count = count_depth_increases_sliding_window(real_input)
+    assert expected_count == actual_count
+    
